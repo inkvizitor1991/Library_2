e3668b93c8f718e15d117f93ee513e2be562ad8a
inkvizitor1991 <85788805+inkvizitor1991@users.noreply.github.com>
2021-11-24 17:39:39 +0300

    fixed the download of the book description

--- library.py
+++ library.py
@@ -179,4 +179,4 @@ if __name__ == '__main__':
             except requests.HTTPError:
                 logging.error('Произошла ошибка при скачивании.')
 
-        download_description_book(book_description, json_path)
+    download_description_book(book_description, json_path)
